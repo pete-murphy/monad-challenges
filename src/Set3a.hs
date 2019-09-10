071de21723f5499271d26702b3bb203eb830e9cb
Peter Murphy <pete@simspace.com>
2019-09-10 01:54:30 -0400

    Fix order

--- src/Set3a.hs
+++ src/Set3a.hs
@@ -5,9 +5,9 @@ module Set3a where
 import           MCPrelude
 
 allPairs :: [a] -> [b] -> [(a, b)]
-allPairs [] _ = []
-allPairs _ [] = []
-allPairs xs@(x:xs') ys@(y:ys') = ((x, y) : allPairs xs' ys) ++ allPairs [x] ys'
+allPairs [] _                  = []
+allPairs _ []                  = []
+allPairs xs@(x:xs') ys@(y:ys') = (x, y) : allPairs [x] ys' ++ allPairs xs' ys
 
 allPairs' :: [a] -> [b] -> [(a, b)]
 allPairs' as bs = concat $ map (\a -> map (\b -> (a, b)) bs) as
