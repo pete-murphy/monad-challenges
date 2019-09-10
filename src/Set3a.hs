a69e55d37a6a2eb7fcd9f135c23dec166ab37ee9
Peter Murphy <pete@simspace.com>
2019-09-10 01:51:51 -0400

    Start Set3a

---
+++ src/Set3a.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Set3a where
+
+import           MCPrelude
+
+allPairs :: [a] -> [b] -> [(a, b)]
+allPairs [] _ = []
+allPairs _ [] = []
+allPairs xs@(x:xs') ys@(y:ys') = ((x, y) : allPairs xs' ys) ++ allPairs [x] ys'
+
+allPairs' :: [a] -> [b] -> [(a, b)]
+allPairs' as bs = concat $ map (\a -> map (\b -> (a, b)) bs) as
